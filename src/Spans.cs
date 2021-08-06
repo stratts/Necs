c093d033a36148d8388a4cfa0c3f5aa71be69dea
Stratton Sloane <stratton.s@outlook.com>
2021-08-06 21:53:28 +1000

    Add reversed span struct

--- src/Spans.cs
+++ src/Spans.cs
@@ -2,6 +2,37 @@ using System;
 
 namespace Necs
 {
+    public static class SpanExtensions
+    {
+        public static ReversedSpan<T> Reversed<T>(this Span<T> span) => new ReversedSpan<T>(span);
+    }
+
+    public ref struct ReversedSpan<T>
+    {
+        private Span<T> _data;
+        private int _idx;
+
+        public int Length => _data.Length;
+
+        public ReversedSpan(Span<T> span)
+        {
+            _data = span;
+            _idx = span.Length;
+        }
+
+        public ref T Current => ref _data[_idx];
+
+        public ref T this[int idx] => ref _data[_data.Length - 1 - idx];
+
+        public bool MoveNext()
+        {
+            _idx--;
+            return _idx >= 0 ? true : false;
+        }
+
+        public ReversedSpan<T> GetEnumerator() => new(_data);
+    }
+
     public ref struct SortedSpan<T>
     {
         private Span<int> _map;
